608d1f19553701708e4d960ac23d72aef5c63700
Aaron Chu <aaron.apps.dev@gmail.com>
2020-08-16 18:05:12 +0800

    Add crash demo code

--- android/app/src/main/kotlin/com/example/hybrid_composition_crash_demo/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/hybrid_composition_crash_demo/MainActivity.kt
@@ -1,6 +1,14 @@
 package com.example.hybrid_composition_crash_demo
 
+import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        flutterEngine
+                .platformViewsController
+                .registry
+                .registerViewFactory("hybrid-view-type", TextPlatformViewFactory())
+    }
 }
